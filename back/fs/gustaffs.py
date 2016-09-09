9ae038088eab7fc9ef64efb88af52da31b064f07
rkohser <rkohser@gmail.com>
2016-09-09 14:55:42 +0200

    gustaffs is a celery app

--- back/fs/gustaffs.py
+++ back/fs/gustaffs.py
@@ -1,3 +1,5 @@
+from celery import Celery
+
 from fs.filefinder import FileFinder
 from fs.filemover import FileMover
 from fs.filesubtitler import FileSubtitler
@@ -5,14 +7,23 @@ from fs.filesubtitler import FileSubtitler
 import common.configurator as config
 
 
-if __name__ == '__main__':
+#Specify mongodb host and datababse to connect to
+BROKER_URL = 'mongodb://localhost:27017/gustaf_fs'
 
+celery_fs = Celery('gustaf_fs',broker=BROKER_URL)
+
+#Loads settings for Backend to store results of jobs 
+celery_fs.config_from_object('celeryconfig')
+
+@celery_fs.task
+def refresh():
     settings = config.get()
 
-    ff = FileFinder(settings['source_dirs'], ('mkv', 'avi', 'mp4'))
+    ff = FileFinder(settings['source_dirs'], settings['source_extensions'])
     fm = FileMover(settings['dest_dir'])
     fs = FileSubtitler(settings['subtitle_languages'], settings['subtitle_providers']);
 
     episodes = ff.find()
     fm.move(episodes, keep_original=True)
     fs.subtitle(episodes)
+
