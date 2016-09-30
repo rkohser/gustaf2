223a71e617147de539e021b589ae2c6650876b95
rkohser <rkohser@gmail.com>
2016-09-30 22:37:59 +0200

    rest service to handle /refresh route and call fs interface

--- back/rest/gustafrest.py
+++ back/rest/gustafrest.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from fs.gustaffs import refresh as fs_refresh
 
 
 gustafrest = Flask(__name__)
 
 @gustafrest.route("/refresh")
 def refresh():
+
+    fs_refresh.delay()
+
     return "Hello World!"
 
 
 if __name__ == "__main__":
-    gustafrest.run()
+    gustafrest.run(port=5000, debug=True)
