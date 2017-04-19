9fc12915dc550048a9fe6699670cab2efe3f547b
rkohser <rkohser@gmail.com>
2017-04-19 16:31:27 +0200

    added endpoint to collect shows list through mongo aggregation

--- back/db/settings.py
+++ back/db/settings.py
@@ -35,6 +35,16 @@ DOMAIN = {
                 'type': 'string'
             }
         }
+    },
+    'shows': {
+        'datasource': {
+            'source': 'episodes',
+            'aggregation': {
+                'pipeline': [
+                    {'$group': {'_id':'$title'}}
+                ]
+            }
+        }
     }
 }
 
