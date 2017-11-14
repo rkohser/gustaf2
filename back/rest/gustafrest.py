e272b00d158f386e7875ffff8a5388f2d5cfea7d
rkohser <rkohser@gmail.com>
2017-11-14 21:45:02 +0100

    began playlist generation

--- back/rest/gustafrest.py
+++ back/rest/gustafrest.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from fs.gustaffs import refresh as fs_refresh
+from flask.helpers import send_file
+
+from rest.playlistgenerator import PlaylistGenerator
 
 
 gustafrest = Flask(__name__)
@@ -10,6 +13,16 @@ def refresh():
     fs_refresh.delay()
 
     return "Hello World!"
+
+@gustafrest.route("/playlist")
+def playlist():
+    
+    show = request.args.get('show')
+    
+    pg = PlaylistGenerator(show)
+    pg.generate();
+    
+    return send_file(r'E:\workspace\gustaf2\xspfplaylist.xspf', as_attachment=True)
 
 
 if __name__ == "__main__":
---
+++ back/rest/playlistgenerator.py
@@ -0,0 +1,20 @@
+'''
+Created on 14 nov. 2017
+
+@author: roland
+'''
+
+class PlaylistGenerator(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, show):
+        '''
+        Constructor
+        '''
+        self.show = show
+        
+    def generate(self):
+        pass
